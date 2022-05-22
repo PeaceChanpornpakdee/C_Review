7a2cf7bf0d7e1edc9a36133fe01f04cb4514d96d
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-05-23 05:58:30 +0700

    Pointer

---
+++ 7_Pointer/7_pointer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+//Function Overloading
+
+void show(int *c) //Passing Pointer 
+{
+    cout << "\" " << c << " \"" << endl;
+}
+
+void show(int &d) //Pass by Reference
+{
+    d = 5555;
+    cout << "\" " << d << " \"" << endl;
+}
+
+void show(char e) //Pass by Value
+{
+    e = '$';
+    cout << "\" " << e << " \"" << endl;
+}
+
+int main() 
+{
+  int a  = 10;
+  int *p = &a; 
+  cout << p << " : " << a << endl;
+  a = 999999;
+  cout << p << " : " << a << endl;
+  show(p);
+  cout << endl;
+
+  int q[10];
+  show(q);
+  cout << q << '\n' << endl;
+
+  show(a);
+  cout << a << '\n' << endl;
+
+  char r = 'S';
+  show(r);
+  cout << r << '\n' << endl;
+
+  return 0;
+}
